22be487b82351dfeb6794b5648a878b2fd95dfdc
JAVEEDJAVEE24 <javeedjavee242GMAIL.COM>
2026-05-09 21:34:01 +0530

    fix backend cors security

--- frontend/src/app/image-preview/image-preview.component.ts
+++ frontend/src/app/image-preview/image-preview.component.ts
@@ -1,36 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { ApiService } from "../services/api.service";
+import { Component, Input } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
-  selector: "app-image-preview",
+  selector: 'app-image-preview',
   templateUrl: './image-preview.component.html',
   styleUrls: ['./image-preview.component.css']
 })
-export class ImagePreviewComponent implements OnInit {
+export class ImagePreviewComponent {
 
-  image: any;
+  @Input() image: any;
 
-  constructor(
-    private route: ActivatedRoute,
-    public api: ApiService
-  ) {}
-
-ngOnInit() {
-
-  const code = this.route.snapshot.paramMap.get("id");
-
-  this.api.getImageByCode(code).subscribe((data: any) => {
-
-    this.image = data;
-
-  });
-
-}
-
-  goBack() {
-    window.history.back();
-  }
-
+  constructor(public api: ApiService) {}
 
 }
