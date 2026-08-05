9d023fb79a21991f1008898fd6ec0e67ce92f704
JAVEEDJAVEE24 <javeedjavee242GMAIL.COM>
2026-08-05 11:32:58 +0530

    Fix production API URL to HTTPS

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://34.194.188.175/api',
-  baseUrl: 'http://34.194.188.175'
+  apiUrl: 'https://www.globaldigipic.com/api',
+  baseUrl: 'https://www.globaldigipic.com'
 };
